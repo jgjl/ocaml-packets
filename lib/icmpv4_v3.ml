2dd6bd9df0301f1cd342cb957b0ca46f62f744cd
Jeremias Blendin <jeremias@blendin.de>
2016-04-09 12:26:38 +0200

    Complete (again), the API should be OKish now, at least for use cases like NAT. Csum checking is still missing as well as tests.

--- lib/icmpv4_v3.ml
+++ lib/icmpv4_v3.ml
@@ -1,4 +1,17 @@
-  
+
+module Option : sig
+  val bind : 'a option -> ('a -> 'b option) -> 'b option
+  val (>>=) : 'a option -> ('a -> 'b option) -> 'b option
+  val return : 'a -> 'a option
+end = struct
+  let bind x f = match x with
+  | Some x -> f x
+  | None -> None
+
+  let return x = Some x
+
+  let (>>=) = bind
+end
 
 let protocol_no = Protocols.Internet (Stdint.Uint8.of_int 1)
 
@@ -14,15 +27,96 @@ module WireV2 = struct
   } [@@big_endian]
   ]
 
-  module Echo = struct
-    [%%cstruct
-    type icmpv4 = {
-      ty:     uint8_t;
-      code:   uint8_t;
-      csum:   uint16_t;
-    } [@@big_endian]
-    ]
-  end
+  (* Echo Request/Reply
+   * Type=0,8; Code=0 *)
+  [%%cstruct
+  type part_echo = {
+    identifier: uint16_t;
+    sequence_no: uint16_t;
+  } [@@big_endian]
+  ]
+
+  (* Destination Unreachable
+   * Type=3; Code=0-15
+   *)
+  [%%cstruct
+  type part_unreachable = {
+    unused: uint16_t;
+    next_hop_mtu: uint16_t;
+  } [@@big_endian]
+  ]
+
+  (* Source Quench
+   * Type=4; Code=4
+   * Time Exceeded
+   * Type=11; Code=0,1
+   * Router Solizitation
+   * Type=10; Code=0
+   *)
+  [%%cstruct
+  type part_unused = {
+    unused: uint32_t;
+  } [@@big_endian]
+  ]
+
+  (* Redirect
+   * Type: 5; Code=0-3 *)
+  [%%cstruct
+  type part_redirect = {
+    ipv4_address: uint32_t;
+  } [@@big_endian]
+  ]
+
+  (* Router Advertisement
+   * Type: 9; Code=0,16 *)
+  [%%cstruct
+  type part_router_advertisement = {
+    advertisement_count: uint8_t;
+    address_entry_size: uint8_t;
+    lifetime: uint16_t;
+  } [@@big_endian]
+  ]
+
+  (* Bad IP header
+   * Type: 12; Code=0-2 *)
+  [%%cstruct
+  type part_bad_ip_header = {
+    pointer: uint8_t;
+    unused: uint8_t;
+    unused: uint16_t;
+  } [@@big_endian]
+  ]
+
+  (* Timestamp Request/Reply
+   * Type=13,14; Code=0
+   *)
+  [%%cstruct
+  type part_timestamp = {
+    identifier: uint16_t;
+    sequence_no: uint16_t;
+    originate_timestamp: uint32_t;
+    receive_timestamp: uint32_t;
+    transmit_timestamp: uint32_t;
+  } [@@big_endian]
+  ]
+
+  (* Address Mask Request/Reply
+   * Type=17, 18; Code=0
+   *)
+  [%%cstruct
+  type part_address_mask = {
+    address_mask: uint32_t;
+  } [@@big_endian]
+  ]
+
+  (* Router Advertisement Entry
+   * Type: 9; Code=0,16 *)
+  [%%cstruct
+  type part_router_advertisement_entry = {
+    router_address: uint32_t;
+    preference_level: uint32_t;
+  } [@@big_endian]
+  ]
 
   [%%cenum
   type destination_unreachable_code =
@@ -90,18 +184,99 @@ module WireV2 = struct
     | ZERO                     [@id 0]
   [@@uint8_t] ]
 
+  let sizeof_part_router_advertisement_total v =
+    let count = get_part_router_advertisement_advertisement_count in
+    let size = get_part_router_advertisement_address_entry_size in
+    size * count + sizeof_part_router_advertisement
+
   type code =
-    | ECHO_REPLY of default_code
-    | DESTINATION_UNREACHABLE of destination_unreachable_code
-    | SOURCE_QUENCH of default_code
-    | REDIRECT_MESSAGE of redirect_message_code
-    | ECHO_REQUEST of default_code
-    | ROUTER_ADVERTISEMENT of router_advertisement_code
-    | ROUTER_SOLICITATION of default_code
-    | TIME_EXCEEDED of time_exceeded_code
-    | BAD_IP_HEADER of bad_ip_header_code
-    | TIMESTAMP of default_code
-    | TIMESTAMP_REPLY of default_code
+    | ECHO_REPLY_CODE of default_code
+    | DESTINATION_UNREACHABLE_CODE of destination_unreachable_code
+    | SOURCE_QUENCH_CODE of default_code
+    | REDIRECT_MESSAGE_CODE of redirect_message_code
+    | ECHO_REQUEST_CODE of default_code
+    | ROUTER_ADVERTISEMENT_CODE of router_advertisement_code
+    | ROUTER_SOLICITATION_CODE of default_code
+    | TIME_EXCEEDED_CODE of time_exceeded_code
+    | BAD_IP_HEADER_CODE of bad_ip_header_code
+    | TIMESTAMP_CODE of default_code
+    | TIMESTAMP_REPLY_CODE of default_code
+
+  let sizeof_ty ty v =
+    sizeof_header + match ty with
+    | ECHO_REPLY -> sizeof_part_echo
+    | DESTINATION_UNREACHABLE -> sizeof_part_unreachable
+    | SOURCE_QUENCH -> sizeof_part_unused
+    | REDIRECT_MESSAGE -> sizeof_part_redirect
+    | ECHO_REQUEST -> sizeof_part_echo
+    | ROUTER_ADVERTISEMENT -> sizeof_part_router_advertisement_total v
+    | ROUTER_SOLICITATION -> sizeof_part_unused
+    | TIME_EXCEEDED -> sizeof_part_unused
+    | BAD_IP_HEADER -> sizeof_part_bad_ip_header
+    | TIMESTAMP -> sizeof_part_timestamp
+    | TIMESTAMP_REPLY -> sizeof_part_timestamp
+
+  let next_protocol ty code v =
+    match ty with
+    | ECHO_REPLY -> Protocols.Unknown
+    | DESTINATION_UNREACHABLE -> Protocols.Protocol Ipv4.Ipv4_packet.protocol_no
+    | SOURCE_QUENCH -> Protocols.Protocol Ipv4.Ipv4_packet.protocol_no
+    | REDIRECT_MESSAGE -> Protocols.Protocol Ipv4.Ipv4_packet.protocol_no
+    | ECHO_REQUEST -> Protocols.Unknown
+    | ROUTER_ADVERTISEMENT -> Protocols.None
+    | ROUTER_SOLICITATION -> Protocols.None
+    | TIME_EXCEEDED -> Protocols.Protocol Ipv4.Ipv4_packet.protocol_no
+    | BAD_IP_HEADER -> Protocols.Protocol Ipv4.Ipv4_packet.protocol_no
+    | TIMESTAMP -> Protocols.None
+    | TIMESTAMP_REPLY -> Protocols.None
+
+  let int_to_code ty code =
+    match ty with
+    | ECHO_REPLY -> 
+        (match int_to_default_code code with
+        | Some c -> Some (ECHO_REPLY_CODE c)
+        | None -> None)
+    | DESTINATION_UNREACHABLE ->
+        (match int_to_destination_unreachable_code code with
+        | Some c -> Some (DESTINATION_UNREACHABLE_CODE c)
+        | None -> None)
+    | SOURCE_QUENCH ->
+        (match int_to_destination_unreachable_code code with
+        | Some c -> Some (DESTINATION_UNREACHABLE_CODE c)
+        | None -> None)
+    | REDIRECT_MESSAGE ->
+        (match int_to_redirect_message_code code with
+        | Some c -> Some (REDIRECT_MESSAGE_CODE c)
+        | None -> None)
+    | ECHO_REQUEST ->
+        (match int_to_default_code code with
+        | Some c -> Some (ECHO_REQUEST_CODE c)
+        | None -> None)
+    | ROUTER_ADVERTISEMENT ->
+        (match int_to_router_advertisement_code code with
+        | Some c -> Some (ROUTER_ADVERTISEMENT_CODE c)
+        | None -> None)
+    | ROUTER_SOLICITATION ->
+        (match int_to_default_code code with
+        | Some c -> Some (ROUTER_SOLICITATION_CODE c)
+        | None -> None)
+    | TIME_EXCEEDED ->
+        (match int_to_time_exceeded_code code with
+        | Some c -> Some (TIME_EXCEEDED_CODE c)
+        | None -> None)
+    | BAD_IP_HEADER ->
+        (match int_to_bad_ip_header_code code with
+        | Some c -> Some (BAD_IP_HEADER_CODE c)
+        | None -> None)
+    | TIMESTAMP ->
+        (match int_to_default_code code with
+        | Some c -> Some (TIMESTAMP_CODE c)
+        | None -> None)
+    | TIMESTAMP_REPLY ->
+        (match int_to_default_code code with
+        | Some c -> Some (TIMESTAMP_REPLY_CODE c)
+        | None -> None)
+
 
   type icmpv4_packet =
     {
@@ -111,290 +286,16 @@ module WireV2 = struct
     next_protocol: Protocols.next_protocol;
     }
 
+  let parse_packet v =
+    Option.(
+    int_to_ty (get_header_ty v) >>= fun ty ->
+    int_to_code ty (get_header_code v) >>= fun code ->
+    (* TODO: Checksum calculation *)
+    let size = sizeof_ty ty v in
+    let next_protocol = next_protocol ty code v in
+     return ({ty=ty;
+              code=code;
+              length=size;
+              next_protocol=next_protocol})
+    )
 end
-
-(*
-  module type Icmpv4_type = sig
-    val ty : ty
-    type code
-    type field
-    val get_field : field -> int
-    val set_field : field -> int -> unit
-    val sizeof : Cstruct.t -> int
-    val get_next_protocol : Cstruct.t -> Protocols.next_protocol
-  end
-
-  type fields =
-    | Type
-    | Code
-    | Csum
-    | Identifier
-    | Sequence_no
-    | Next_hop_mtu
-    | Payload
-    | Timestamp
-    | Adv_cnt
-    | Adv_entry_size
-    | Lifetime
-    | Router_address
-    | Preference_lvl
-
-  type ('a, 'b) icmpv4_type = {
-    codes: int list;
-    len: int;
-    fields: 'a list;
-    next_part: Protocols.next_protocol;
-    get: 'a -> 'b;
-    set: 'a -> 'b -> unit;
-  }
-
-  type types =
-    | Echo of [`identifier | `sequence_no]
-    | Dst_unreach of [`next_hop_mtu]
-    | Empty
-    | Redir_msg of [`address]
-    | Rtr_adv of [`addr_cnt|`addr_entry_size|`lifetime|`rtr_addr|`rtr_pref_lvl]
-    | Bad_ip_hdr of [`pointer]
-    | Timestamp of [`identifier|`sequence_no|`orig_ts|`rx_ts|`tx_ts]
-
-  let get ty =
-    match ty with
-    | ECHO_REPLY -> 
-      (fun field v -> match field with
-        | Echo `identifier -> Cstruct.BE.get_uint16 v 0
-        | Echo `sequence_no -> Cstruct.BE.get_uint16 v 2)
-    | DESTINATION_UNREACHABLE -> fun field v -> match field with
-                      | Dst_unreach `next_hop_mtu -> Cstruct.BE.get_uint16 v 2
-
-  let set ty =
-    match ty with
-    | ECHO_REPLY -> (fun field value -> function
-                      | `identifier -> Cstruct.BE.set_uint16 value)
-
-  let get_type ty =
-    let identifier =    {offset=0; size=2; name="identifier"} in
-    let sequence_no =   {offset=0; size=2; name="sequence number"} in
-    let next_hop_mtu =  {offset=2; size=2; name="next hop mtu"} in
-    let pointer =       {offset=0; size=1; name="pointer"} in
-    let address =       {offset=0; size=2; name="sequence number"} in
-    let orig_ts =       {offset=4; size=4; name="originate timestamp"} in
-    let rx_ts =         {offset=8; size=4; name="receive timestamp"} in
-    let tx_ts =         {offset=4; size=4; name="transmit timestamp"} in
-    let ad_cnt =        {offset=0; size=1; name="advertisement count"} in
-    let ad_entry_size = {offset=1; size=1; name="advertisement entry size"} in
-    let lifetime =      {offset=2; size=2; name="lifetime"} in
-    let rtr_addr =      {offset=4; size=4; name="router address"} in
-    let rtr_pref_lvl =  {offset=8; size=4; name="preference level"} in
-    let ipproto = Protocols.Protocol Ipv4.Ipv4_packet.protocol_no in
-    match ty with
-    | ECHO_REPLY -> {codes=[0]; len=4;
-                    fields=[`identifier; `sequence_no;];
-                    next_part=Protocols.Unknown;
-                    get=(get ty);
-                    set=(set ty);
-                    }
-    | DESTINATION_UNREACHABLE -> {name="Destination unreachable";
-                    max_code=15; length=4;
-                    fields=[next_hop_mtu];
-                    next_part=ipproto}
-    | SOURCE_QUENCH -> {name="Unused";
-                    max_code=0; length=4;
-                    fields=[];
-                    next_part=ipproto}
-    | REDIRECT_MESSAGE -> {name="Redirect message";
-                    max_code=3; length=4;
-                    fields=[address];
-                    next_part=ipproto}
-    | ECHO_REQUEST -> {name="Echo reply";
-                    max_code=0; length=4;
-                    fields=[identifier; sequence_no];
-                    next_part=Protocols.Unknown}
-    | ROUTER_ADVERTISEMENT -> {name="Router advertisement";
-                    max_code=16; length=12;
-                    fields=[ad_cnt; ad_entry_size; lifetime; rtr_addr; rtr_pref_lvl];
-                    next_part=Protocols.None}
-    | ROUTER_SOLICITATION -> {name="Router solicitation";
-                    max_code=0; length=4;
-                    fields=[];
-                    next_part=Protocols.None}
-    | TIME_EXCEEDED -> {name="Time exceeded";
-                    max_code=1; length=4;
-                    fields=[];
-                    next_part=ipproto}
-    | BAD_IP_HEADER -> {name="Bad IP header";
-                    max_code=2; length=4;
-                    fields=[pointer];
-                    next_part=ipproto}
-    | TIMESTAMP -> {name="Timestamp";
-                    max_code=0; length=16;
-                    fields=[identifier;sequence_no;orig_ts;rx_ts;tx_ts];
-                    next_part=Protocols.None}
-    | TIMESTAMP_REPLY -> {name="Timestamp reply";
-                    max_code=0; length=16;
-                    fields=[identifier;sequence_no;orig_ts;rx_ts;tx_ts];
-                    next_part=Protocols.None}
-
-  let get_payload ty_def v =
-    let shiftlen = sizeof_icmpv4 + ty_def.length in
-    if (Cstruct.len v) > shiftlen
-    then Some (Cstruct.shift v shiftlen)
-    else None
-
-  let is_valid v ty code tys =
-    let type_code_ok = ty <= ty && code <= tys.max_code
-    in
-    let length_ok = (sizeof_icmpv4 + tys.length) <= Cstruct.len v in
-    (* let csum_ok = ... *)
-    type_code_ok && length_ok
-
-  (*TODO: Checksum calculation *)
-end
-
-
-
-module Parser = struct
-
-  type fields =
-    | Type
-    | Code
-    | Csum
-    | Identifier
-    | Sequence_no
-    | Next_hop_mtu
-    | Payload
-
-  type icmpv4 = {
-    ty: Stdint.uint8;
-    code: Stdint.uint8;
-    csum: Stdint.uint16;
-  }
-
-  type option_unreachable = {
-    ip_orig_proto: Stdint.uint8;
-  }
-
-  type option_unreachable_too_big = {
-    next_hop_mtu: Stdint.uint8;
-    ip_orig_proto: Stdint.uint8
-    }
-
-  type icmpv4_echo = {
-    identifier: int;
-    sequence_number: int;
-    data: Cstruct.t;
-  }
-
-  type icmpv4_time_exceeded = {
-    code: int;
-    ip_orig_src: Stdint.uint32;
-    ip_orig_dst: Stdint.uint32;
-    ip_orig_proto: int
-  }
-
-  (* Based on Wikipedia *cough* *)
-  let type_to_string ty code =
-    match ty with
-    | ECHO_REPLY when code=0 -> Some "Echo reply"
-    | ECHO_REQUEST when code=0 -> Some "Echo request"
-    | DESTINATION_UNREACHABLE when code < 16->
-      (let msg = "Redirect message: " in
-      match code with
-      | 0  -> Some (msg ^ "Destination network unreachable")
-      | 1  -> Some (msg ^ "Destination host unreachable")
-      | 2  -> Some (msg ^ "Destination protocol unreachable")
-      | 3  -> Some (msg ^ "Destination port unreachable")
-      | 4  -> Some (msg ^ "Fragmentation required, and DF flag set")
-      | 5  -> Some (msg ^ "Source route failed")
-      | 6  -> Some (msg ^ "Destination network unknown")
-      | 7  -> Some (msg ^ "Destination host unknown")
-      | 8  -> Some (msg ^ "Source host isolated")
-      | 9  -> Some (msg ^ "Network administratively prohibited")
-      | 10 -> Some (msg ^ "Host administratively prohibited")
-      | 11 -> Some (msg ^ "Network unreachable for TOS")
-      | 12 -> Some (msg ^ "Host unreachable for TOS")
-      | 13 -> Some (msg ^ "Communication administratively prohibited")
-      | 14 -> Some (msg ^ "Host Precedence Violation")
-      | 15 -> Some (msg ^ "Precedence cutoff in effect"))
-    | SOURCE_QUENCH when code=0 -> Some "Source quench (message type is deprecated!)"
-    | REDIRECT_MESSAGE when code<4->
-      (let msg = "Redirect message: " in
-      match code with
-      | 0 -> Some (msg ^ "Redirect for Network")
-      | 1 -> Some (msg ^ "Redirect for Host")
-      | 2 -> Some (msg ^ "Redirect for Type of Service and Network")
-      | 3 -> Some (msg ^ "Redirect for Type of Service and Host"))
-    | ROUTER_ADVERTISEMENT when code=0 -> Some "Router advertisement"
-    | ROUTER_SOLICITATION when code=0 -> Some "Router solicitation"
-    | TIME_EXCEEDED when code=0 -> Some "Router solicitation"
-    | BAD_IP_HEADER when code<3 ->
-      (let msg = "Bad IP header: " in
-      match code with
-      | 0 -> Some (msg ^ "Pointer indicates this error")
-      | 1 -> Some (msg ^ "Missing a required option")
-      | 2 -> Some (msg ^ "Bad length"))
-    | TIMESTAMP when code=0 -> Some "Timestamp"
-    | TIMESTAMP_REPLY when code=0 -> Some "Timestamp reply"
-    | _ -> None
-
-  type structure =
-    | Field of fields
-    | Option of fields list
-    | Payload of Protocols.next_protocol
-
-  type icmpv4_msg =
-    | Dst_unreachable of icmpv4_unreachable * Cstruct.t
-    | Dst_unreachable_too_big of icmpv4_unreachable_too_big * Cstruct.t
-    | Echo_request of icmpv4_echo
-    | Echo_reply of icmpv4_echo
-    | Time_exceeded of icmpv4_time_exceeded * Cstruct.t
-
-  type part = {
-    name: string;
-    fields: fields list;
-    length: int;
-  }
-  let get_fields part =
-    match part with
-    | Icmpv4 -> {fields = [Type, Code, Csum], length = 4}
-    | Opt_unreachable_too_big -> {fields=[Next_hop_mtu], length = 4}
-    | Opt_unreachable -> {fields = [], length = 4}
-    | Opt_echo -> {fields = [Identifier; Sequence_no]; length = 4}
-    | Opt_unknown -> {fields = []; length = 4}
-
-  let get_field field v =
-    match field with
-    | Type -> Cstruct.get_uint8 v 0
-    | Code -> Cstruct.get_uint8 v 1
-    | Csum -> Cstruct.BE.get_uint16 v 2
-    | Identifier -> Cstruct.BE.get_uint16 v 0
-    | Sequence_no ->  Cstruct.BE.get_uint16 v 2
-    | Next_hop_mtu -> Some (Cstruct.BE.get_uint16 v 2)
-
-  let get_option (icmpv4_type, icmpv4_code) field v =
-    match field, icmpv4_type, icmpv4_code with
-    | Identifier, 0, 0 | Identifier, 8, 0 -> Some (Cstruct.BE.get_uint16 v 4)
-    | Sequence_no, 0, 0 | Sequence_no, 8, 0 -> Some (Cstruct.BE.get_uint16 v 6)
-    | Next_hop_mtu, 3, 4 -> Some (Cstruct.BE.get_uint16 v 6)
-    | _, _, _ -> None
-
-  let fields = [Type, Code, Csum]
-
-  (*
-  let next_part current_part v =
-    | 3, 4 -> [Field Type; Field Code; Field Csum; OptionalField Next_hop_mtu; Payload (Protocols.Protocol Ipv4.Ipv4_packet.protocol_no)]
-    | 3, _ -> [Field Type; Field Code; Field Csum; Payload (Protocols.Protocol Ipv4.Ipv4_packet.protocol_no)]
-    | 0, 0 | 8, 0 -> [Field Type; Field Code; Field Csum; OptionalField Identifier; OptionalField Sequence_no; Payload Protocols.Unknown]
-    | _, _ -> [Field Type; Field Code; Field Csum]
-    *)
-
-
-  let get_payload icmpv4_type icmpv4_code v =
-    match icmpv4_type, icmpv4_code with
-    (*| Stdint.Uint8.zero, Stdint.Uint8.zero -> Cstruct.shift v 4*)
-    | 3,_ -> Protocols.Protocol Ipv4.Ipv4_packet.protocol_no, Some (Cstruct.shift v 8)
-    | 8,0 | 0,0 -> Protocols.Unknown, Some (Cstruct.shift v 8)
-    | _, _ -> Protocols.None, None
-    *)
-end
-
-*)
