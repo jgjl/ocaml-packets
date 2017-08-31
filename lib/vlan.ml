1522dc595d569164f18ebbe85e5ecfce22057992
jgjl <gh@jgjl.de>
2017-08-31 23:05:29 +0200

    Added VLAN reading.

--- lib/vlan.ml
+++ lib/vlan.ml
@@ -6,5 +6,8 @@ module Ethernet_packet = struct
     } [@@big_endian]
   ]
 
+  let get_vid buf = (get_vlan_tci buf) land 0b0000_1111_1111_1111
+  let get_pcp buf = (get_vlan_tci buf) lsr 13
+  let get_dei buf = ((get_vlan_tci buf) lsr 12) land 0b0001
 end
 
